68be9308301b102c72a3d6b7b049901ff169d33f
John Ray <j.logan.ray@gmail.com>
2020-01-01 20:39:35 -0800

    update image attributes found in svg

--- R/pixelate.R
+++ R/pixelate.R
@@ -5,5 +5,10 @@
 #' @examples
 #' @export
 pixelate <- function(img_path){
-  img_path
+  img <- image_read('http://jeroen.github.io/images/tiger.svg')
+  
+  info <- image_info(img)
+  
+  w <- info$width
+  h <- info$h
 }
