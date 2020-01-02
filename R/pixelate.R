433e55151b30596fdef16f4ace94eb2c48506d32
John Ray <j.logan.ray@gmail.com>
2020-01-01 21:17:26 -0800

    magick to cimg workflow

--- R/pixelate.R
+++ R/pixelate.R
@@ -4,11 +4,18 @@
 #' @return 
 #' @examples
 #' @export
-pixelate <- function(img_path){
+#pixelate <- function(img_path, resolution){
   img <- image_read('http://jeroen.github.io/images/tiger.svg')
   
   info <- image_info(img)
   
+  the_cimg <- magick2cimg(img)
+  
   w <- info$width
   h <- info$h
-}
+  
+  winc <- round(w/256, 0)
+  hinc <- round(h/256, 0)
+  
+  imgradient(img, "x", scheme = 3)
+#}
