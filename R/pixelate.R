926c489341205bf5a3e651dc7a6dbd4a07bedaac
John Ray <j.logan.ray@gmail.com>
2020-01-09 23:14:40 -0800

    marginally less overthinking

--- R/pixelate.R
+++ R/pixelate.R
@@ -18,7 +18,7 @@ pixelate <- function(img_path, resolution){
   #cimg_df <- as.data.frame(the_cimg)
   
   w <- info$width
-  h <- info$h
+  h <- info$height
   
   winc <- round(w/256, 0)
   hinc <- round(h/256, 0)
@@ -31,12 +31,14 @@ pixelate <- function(img_path, resolution){
   xstart <- seq(1, w, by = winc*10)
   ystart <- seq(1, h, by = hinc*10)
   
+  res_mat <- matrix(nrow = length(xstart), ncol = length(ystart))
+  
   # find the average of each chunk
   for(i in 1:length(xstart)){
     for(j in 1:length(ystart)){
       crop_statement <- paste0(winc, "x", hinc, "+", xstart[i], "+", ystart[j])
       temp <- image_crop(img, crop_statement) %>% image_quantize(max = 2) %>% image_median(radius = 5)
-      med_col <- as.integer(temp) %>% as.hexmode() %>% as.character() %>% c() %>% table() %>% sort() %>% head(1) %>% names()
+      res_mat[i, j] <- as.integer(temp) %>% as.hexmode() %>% as.character() %>% c() %>% table() %>% sort() %>% head(1) %>% names()
     }
   }
   
