2028044a5202e8f58e6d220f5b291d155558ca0d
John Ray <j.logan.ray@gmail.com>
2020-01-16 23:33:32 -0800

    not sure we need those libraries

--- R/pixelate.R
+++ R/pixelate.R
@@ -73,7 +73,7 @@ pixelate <- function(img_path, resolution){
   ggplot(res, aes(x = Var1, y = Var2*-1, fill = value)) +
     geom_tile() +
     geom_text(aes(label = value), color = 'black') +
-    scale_fill_manual(values = unique(res$value)) +
+    scale_fill_manual(values = unique(res$value), aesthetics = 'identity') +
     theme_void() +
     theme(legend.position = 'null')
   
