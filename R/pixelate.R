5efe10de22a01bd22c77f0133c34a0a6cdc21221
John Ray <j.logan.ray@gmail.com>
2020-01-20 22:59:16 -0800

    from rgb to hex

--- R/pixelate.R
+++ R/pixelate.R
@@ -12,66 +12,36 @@ library(ggplot2)
 library(reshape2)
 library(scales)
 
-get_colorPal <- function(im, n=8, cs="RGB"){
-  #print(cs) 
-  tmp <-im %>% image_resize("100") %>% 
-    image_quantize(max=n, colorspace=cs) %>%  ## reducing colours! different colorspace gives you different result
-    magick2cimg() %>%  ## I'm converting, becauase I want to use as.data.frame function in imager package.
-    RGBtoHSV() %>% ## i like sorting colour by hue rather than RGB (red green blue)
-    as.data.frame(wide="c") %>%  #3 making it wide makes it easier to output hex colour
-    mutate(hex=hsv(rescale(c.1, from=c(0,360)),c.2,c.3),
-           hue = c.1,
-           sat = c.2,
-           value = c.3) %>%
-    count(hex, hue, sat,value, sort=T) %>% 
-    mutate(colorspace = cs)
-  
-  return(tmp %>% select(colorspace,hex,hue,sat,value,n)) ## I want data frame as a result.
-  
-}
-
 pixelate <- function(img_path, resolution){
-  img <- image_read('http://jeroen.github.io/images/tiger.svg')
+  info <- image_read('~/Downloads/IMG_20190712_095348.jpg') %>% image_info()
   
-  img_dims <- dim(img)
-  info <- image_info(img)
+  the_cimg <- imager::load.image('~/Downloads/IMG_20190712_095348.jpg') %>% imager::as.cimg()
   
-  the_cimg <- magick2cimg(img)
   cimg_df <- as.data.frame(the_cimg)
   
-  w <- info$width
-  h <- info$height
+  fidelity <- 27
   
-  as_rgbs <- data.frame(
-    r = as.hexmode(as.integer(the_cimg[,,1]*255)),
-    g = as.hexmode(as.integer(the_cimg[,,2]*255)),
-    b = as.hexmode(as.integer(the_cimg[,,3]*255))
-  )
+  ws <- seq(1, info$width, fidelity)
+  hs <- seq(1, info$height, fidelity)
   
-  #res_mat <- matrix(nrow = length(xstart), ncol = length(ystart))
+  samp_mat <- matrix(ncol = length(hs), nrow = length(ws))
   
-  # find the average of each chunk
-  #for(i in 1:length(xstart)){
-    #for(j in 1:length(ystart)){
-      #crop_statement <- paste0(winc, "x", hinc, "+", xstart[i], "+", ystart[j])
-      #temp <- image_crop(img, crop_statement) %>%
-        #image_quantize(max = 1) %>%
-        #image_median(radius = 5)
-
-      #res_mat[i, j] <- (as.integer(temp) %>% as.hexmode() %>% as.character() %>% c() %>% table() %>% sort() %>% head(1) %>% names())
-    #}
-  #}
+  for(i in 1:nrow(samp_mat)){
+    for(j in 1:ncol(samp_mat)){
+      samp_mat[i, j] <- rgb(
+        red = the_cimg[ws[i], hs[j], 1, 1],
+        blue = the_cimg[ws[i], hs[j], 1, 2],
+        green = the_cimg[ws[i] ,hs[j], 1, 3]
+      )
+      
+      print(samp_mat[i, j])
+    }
+  }
   
-  res <- melt(res_mat)
-  res$value <- as.character(res$value)
-  res$value <- paste0('#', substr(res$value, 1, 6))
+  pdat <- reshape2::melt(samp_mat)
   
-  resplot <- ggplot(res, aes(x = Var1, y = Var2*-1, fill = value)) +
-    geom_tile() +
-    geom_text(aes(label = value), color = 'black') +
-    scale_fill_manual(values = unique(res$value), aesthetics = 'identity') +
-    theme_void() +
-    theme(legend.position = 'null')
-  
-  return(resplot)
+  ggplot(pdat, aes(x = Var1, y = Var2)) +
+    geom_raster(aes(fill = value)) +
+    coord_equal() +
+    coord_flip()
 }
