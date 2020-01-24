087c47294643a82727b93136dbb4c8ed268644ed
John Ray <j.logan.ray@gmail.com>
2020-01-23 23:55:39 -0800

    setting up pseudocode for the color simplifier

--- R/pixelate.R
+++ R/pixelate.R
@@ -14,30 +14,31 @@ library(reshape2)
 library(scales)
 library(jpeg)
 
-pixelate <- function(img_path, resolution){
+pixelate <- function(img_path, resolution = 200){
   img <- readJPEG('~/Downloads/IMG_20190712_095348.jpg')
   info <- image_info(image_read('~/Downloads/IMG_20190712_095348.jpg'))
   
-  #the_cimg <- imager::load.image('~/Downloads/IMG_20190712_095348.jpg') %>% imager::as.cimg()
-  
   the_val <- rgb(img[,,1], img[,,2],img[,,3])
   
+  # to smooth colors...
+  # find the uncommon colors
   allcols <- table(the_val)
   
-  colordistance::getColorDistanceMatrix(getHistList('~/Downloads/IMG_20190712_095348.jpg'))
+  # set a smoothing threshold
+  
+  # find nearest color above threshold for every color below threshold
   
   myImg <- matrix(the_val, dim(img)[1], dim(img)[2])
   
-  #cimg_df <- as.data.frame(the_cimg)
-  
-  w_fidelity <- round(info$width * 1/200, 0)
-  h_fidelity <- round(info$height * 1/200, 0)
+  w_fidelity <- round(info$width * 1/resolution, 0)
+  h_fidelity <- round(info$height * 1/resolution, 0)
   
   ws <- seq(1, info$width, w_fidelity)
   hs <- seq(1, info$height, h_fidelity)
   
   myImg <- myImg[hs, ws]
   
-  image(t(matrix(length(as.vector(myImg)):1, nrow = length(hs), ncol = length(ws))), col=as.vector(myImg), xaxt = "n", yaxt = "n")
+  out <- image(t(matrix(length(as.vector(myImg)):1, nrow = length(hs), ncol = length(ws))), col=as.vector(myImg), xaxt = "n", yaxt = "n")
   
+  return(out)
 }
