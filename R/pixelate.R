73ee612a2b3ecb504eabe1b575bc89c20cd99a06
John Ray <j.logan.ray@gmail.com>
2020-04-04 21:28:47 -0700

    rebuilding the image loader

--- R/pixelate.R
+++ R/pixelate.R
@@ -12,39 +12,33 @@ library(ggplot2)
 library(reshape2)
 library(scales)
 library(jpeg)
+library(tidyverse)
 
 pixelate <- function(img_path, resolution = 200, imgpath){
+  resolution <- 30
+  
   img <- readJPEG('~/Desktop/vzxLeexP9rBn9zKfwhXPS6TK_cwIP9kaFnFN1r28dL4.jpg')
   info <- image_info(image_read('~/Desktop/vzxLeexP9rBn9zKfwhXPS6TK_cwIP9kaFnFN1r28dL4.jpg'))
   
-  vals <- matrix(c(img[,,1], img[,,2],img[,,3]), ncol = 3, byrow = F)
-  
-  # find the uncommon colors
-  allcols <- rev(sort(table(vals)))
-  
-  # pick a top n
-  top_n <- names(allcols)[1:200]
-  
-  # minimize that distance
-  for(i in 1:nrow(vals)){
-    closest <- top_n[which.min(abs(sum(vals[, i]) - top_n))]
-    vals[i] <- closest
-  }
-
-  dist <- sqrt((b_col[1] - a_col[1]) + (b_col[2] - a_col[2]) + (b_col[3] - a_col[3]))
-  
-  # grepl the old color to the new color
-  myImg <- matrix(the_val, dim(img)[1], dim(img)[2])
-
   w_fidelity <- round(info$width * 1/resolution, 0)
   h_fidelity <- round(info$height * 1/resolution, 0)
-
+  
   ws <- seq(1, info$width, w_fidelity)
   hs <- seq(1, info$height, h_fidelity)
-
+  
   myImg <- myImg[hs, ws]
-
+  
   out <- image(t(matrix(length(as.vector(myImg)):1, nrow = length(hs), ncol = length(ws))), col=as.vector(myImg), xaxt = "n", yaxt = "n")
+  
+  vals <- matrix(c(img[,,1], img[,,2],img[,,3]), ncol = 3, byrow = F)
+  
+  # pick a top n
+  vals %>%
+    data.frame() %>% 
+    #sample_n(size = 100000) %>%
+    .[order( .[, 1], .[, 2], .[, 3]), ] %>%
+    head()
+
   
   return(out)
 }
